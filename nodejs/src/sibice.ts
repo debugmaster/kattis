672a5ddd05965d52583b4b649a9cfdfa73d9c5d2
Camilo <camilo@camilo.fm>
2018-08-04 20:50:52 -0400

    Tweak 'sibice' solution.

--- nodejs/src/sibice.ts
+++ nodejs/src/sibice.ts
@@ -1,26 +1,26 @@
 import * as Readline from 'readline';
 
-let N: number;
-let W: number;
-let H: number;
+let numberOfMatches: number;
+let boxWidth: number;
+let boxSize: number;
 
 Readline.createInterface({
     input: process.stdin,
     output: process.stdout
 }).on('line', (input: string) => {
-    if (!N) {
-        [N,W,H] = input.split(' ').map((s) => parseInt(s));
+    if (!numberOfMatches) {
+        [numberOfMatches, boxWidth, boxSize] = input.split(' ').map((s) => parseInt(s));
         return;
     }
 
     let matchSize = parseInt(input);
     // It needs to fit horizontally, vertically, or diagonally
-    if (matchSize <= W || matchSize <= H || (matchSize * matchSize <= W*W + H*H)) {
+    if (matchSize <= boxWidth || matchSize <= boxSize || (matchSize * matchSize <= boxWidth*boxWidth + boxSize*boxSize)) {
         console.log('DA');
     } else {
         console.log('NE');
     }
 
-    // Decrease the number of matches to know when N, W, and H will be reset.
-    N--;
+    // Decrease the number of matches to know when variables will be reset.
+    numberOfMatches--;
 });
