53d1d4c2a8c3314e83c8bd6f8f5607d3efbd32e2
Camilo <camilo@camilo.fm>
2019-02-03 14:06:20 -0200

    Fix solution of 'welcomeeasy'.

--- java/src/WelcomeEasy.java
+++ java/src/WelcomeEasy.java
@@ -1,67 +1,50 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+/**
+ * Based on https://github.com/lantoli/Google-Code-Jam/blob/master/06_WelcomeToCodeJam/WelcomeToCodeJamDP.java
+ */
+
 import java.util.Scanner;
 
 public class WelcomeEasy {
 
-  private static final char[] SENTENCE =
-      new char[] {
-        'w', 'e', 'l', 'c', 'o', 'm', 'e', ' ', 't', 'o', ' ', 'c', 'o', 'd', 'e', ' ', 'j', 'a', 'm'
-      };
+  private static final char[] SENTENCE = "welcome to code jam".toCharArray();
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int tests = Integer.parseInt(sc.nextLine());
     for (int test = 1; test <= tests; test++) {
-      String sanitized = sanitize(sc.nextLine());
-      System.out.println(String.format("Case #%s: %04d", test, WelcomeEasy.solve(sanitized)));
+      System.out.println(String.format("Case #%s: %04d", test, WelcomeEasy.solve(sc.nextLine())));
     }
   }
 
-  private static int solveForOneString(String text) {
-    int[] occurrences = new int[SENTENCE.length];
+  private static int solve(String test) {
 
-    int curr = 0;
-    int currPos = text.indexOf(SENTENCE[curr]);
-    while (curr < SENTENCE.length) {
-      int nextPos =
-          curr != SENTENCE.length - 1 ? text.indexOf(SENTENCE[curr + 1], currPos) : text.length();
-      while (currPos >= 0 && currPos < nextPos) {
-        if (text.charAt(currPos) == SENTENCE[curr]) {
-          occurrences[curr]++;
+    int firstIndex = test.indexOf(SENTENCE[0]);
+    if (firstIndex < 0) {
+      return 0;
+    }
+
+    test = test.substring(firstIndex);
+
+    int[] state = null;
+    int[] lastState = null;
+
+    for (int i = 0; i < test.length(); i++) {
+      state = new int[SENTENCE.length];
+      for (int j = 0; j < SENTENCE.length; j++) {
+        if (i > 0) {
+          state[j] = lastState[j];
         }
-        currPos++;
-      }
-      curr++;
-    }
-
-    return Arrays.stream(occurrences).reduce(1, (a, b) -> a * b);
-  }
-
-  private static int solve(String text) {
-    List<String> potentialCases = new ArrayList<>();
-    for (int i = 0; i < text.length(); i++) {
-      if (text.charAt(i) == SENTENCE[0]) {
-        int j;
-        for (j = i; j < text.length(); j++) {
-          if (text.charAt(j) != SENTENCE[0]) {
-            potentialCases.add(text.substring(i, j) + text.substring(j).replace("w", ""));
-            i = j - 1;
-            break;
+        if (test.charAt(i) == SENTENCE[j]) {
+          if (i > 0 && j > 0) {
+            state[j] += lastState[j - 1];
+          } else {
+            state[j]++;
           }
         }
       }
+      lastState = state;
     }
-    return potentialCases.stream().parallel().map(WelcomeEasy::solveForOneString).reduce(0, (a, b) -> a + b);
-  }
 
-  private static String sanitize(String text) {
-    int firstWPos = text.indexOf('w');
-    int lastMPos = text.lastIndexOf('m');
-    if (firstWPos > 0 && lastMPos > 0 && firstWPos < lastMPos) {
-      return text.substring(firstWPos, lastMPos + 1);
-    }
-    return text;
+    return state[SENTENCE.length - 1];
   }
 }
